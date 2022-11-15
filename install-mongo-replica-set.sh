f07a53c1fc0dafbcd406720d2bc4dc7dc8d4abde
Mahdad Ghasemian <mahdad.ghasemian@gmail.com>
2022-11-15 11:18:05 +0330

    docker network

--- install-mongo-replica-set.sh
+++ install-mongo-replica-set.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root user or run with sudo"
-  exit
-fi
-
-## Check for docker
-docker --version
-if [ $? -ne 0 ]
-  then
-    curl -fsSL https://get.docker.com | sh
-fi
-
 ## PORTs
 PORT1=$1
 PORT2=$2
@@ -20,6 +8,13 @@ PORT3=$3
 ## Username and password
 DATABASE_ADMIN_USERNAME=$4
 DATABASE_ADMIN_PASSWORD=$5
+
+## Docker network
+DOCKER_NETWORK=$6
+if [ -z $DOCKER_NETWORK ];
+then
+    DOCKER_NETWORK="mongo-network-"$( tr -cd a-z </dev/urandom | head -c '4' ; echo '' )
+fi
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root user or run with sudo"
@@ -116,10 +111,14 @@ sed -i s/PORT3/$PORT3/g d_cmd.js
 sed -i s/DATABASE_ADMIN_USERNAME/$DATABASE_ADMIN_USERNAME/g d_cmd.js
 sed -i s/DATABASE_ADMIN_PASSWORD/$DATABASE_ADMIN_PASSWORD/g d_cmd.js
 
+## Create Docker network
+docker network create $DOCKER_NETWORK || true
+
 ## Run Docker containers
 docker run -d --restart always \
 	--name mongo1 \
 	-p $PORT1:27017 \
+  --network $DOCKER_NETWORK \
   -e MONGO_INITDB_ROOT_USERNAME=$DATABASE_ADMIN_USERNAME \
 	-e MONGO_INITDB_ROOT_PASSWORD=$DATABASE_ADMIN_PASSWORD \
   --mount src=$PWD/data/data1,target=/data/db,type=bind \
@@ -131,6 +130,7 @@ docker run -d --restart always \
 docker run -d --restart always \
 	--name mongo2 \
 	-p $PORT2:27017 \
+  --network $DOCKER_NETWORK \
   -e MONGO_INITDB_ROOT_USERNAME=$DATABASE_ADMIN_USERNAME \
 	-e MONGO_INITDB_ROOT_PASSWORD=$DATABASE_ADMIN_PASSWORD \
   --mount src=$PWD/data/data2,target=/data/db,type=bind \
@@ -141,6 +141,7 @@ docker run -d --restart always \
 docker run -d --restart always \
 	--name mongo3 \
 	-p $PORT3:27017 \
+  --network $DOCKER_NETWORK \
   -e MONGO_INITDB_ROOT_USERNAME=$DATABASE_ADMIN_USERNAME \
 	-e MONGO_INITDB_ROOT_PASSWORD=$DATABASE_ADMIN_PASSWORD \
   --mount src=$PWD/data/data3,target=/data/db,type=bind \
@@ -158,10 +159,15 @@ docker restart mongo1 mongo2 mongo3
 
 cat <<EOF > ./config.log
   Connection URL :
-  admin url:
-    mongodb://$DATABASE_ADMIN_USERNAME:$DATABASE_ADMIN_PASSWORD@$SYSTEM_IP:$PORT1,$SYSTEM_IP:$PORT2,$SYSTEM_IP:$PORT3/test?replicaSet=rs0&readPreference=primary&ssl=false&authMechanism=DEFAULT&authSource=admin
-  proxy:
-    mongodb://$DATABASE_ADMIN_USERNAME:$DATABASE_ADMIN_PASSWORD@$SYSTEM_IP:$PORT1,$SYSTEM_IP:$PORT2,$SYSTEM_IP:$PORT3/test?replicaSet=rs0&readPreference=primary&ssl=false&authMechanism=DEFAULT&authSource=admin&proxyPort=20170&proxyHost=127.0.0.1
+    admin url:
+      mongodb://$DATABASE_ADMIN_USERNAME:$DATABASE_ADMIN_PASSWORD@$SYSTEM_IP:$PORT1,$SYSTEM_IP:$PORT2,$SYSTEM_IP:$PORT3/test?replicaSet=rs0&readPreference=primary&ssl=false&authMechanism=DEFAULT&authSource=admin
+    proxy:
+      mongodb://$DATABASE_ADMIN_USERNAME:$DATABASE_ADMIN_PASSWORD@$SYSTEM_IP:$PORT1,$SYSTEM_IP:$PORT2,$SYSTEM_IP:$PORT3/test?replicaSet=rs0&readPreference=primary&ssl=false&authMechanism=DEFAULT&authSource=admin&proxyPort=20170&proxyHost=127.0.0.1
+  Docker network: $DOCKER_NETWORK
+  Username: $DATABASE_ADMIN_USERNAME
+  Password: $DATABASE_ADMIN_PASSWORD
+  IP: $SYSTEM_IP
+  Ports: $PORT1 $PORT2 $PORT3
 EOF
 
 cat ./config.log
