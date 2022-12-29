e01fe91af8b7dcf6b170fa0c2c27a35ed526a03d
Mahdad Ghasemian <mahdad.ghasemian@gmail.com>
2022-12-29 13:52:42 +0330

    cmd change

--- install-mongo-replica-set.sh
+++ install-mongo-replica-set.sh
@@ -101,15 +101,9 @@ SYSTEM_IP=$(curl -s "https://api.ipify.org/" )
 ## Write init cmd for database
 cat <<EOF > ./d_cmd.js
 db = (new Mongo('localhost:27017')).getDB('test')
-rs.initiate({"_id":"rs0","members":[{"_id":0,"host":"SYSTEM_IP:PORT1"},{"_id":1,"host":"SYSTEM_IP:PORT2"},{"_id":2,"host":"SYSTEM_IP:PORT3"}]})
+rs.initiate({"_id":"rs0","members":[{"_id":0,"host":"$SYSTEM_IP:$PORT1"},{"_id":1,"host":"$SYSTEM_IP:$PORT2"},{"_id":2,"host":"$SYSTEM_IP:$PORT3"}]})
 show dbs
 EOF
-sed -i s/SYSTEM_IP/$SYSTEM_IP/g d_cmd.js
-sed -i s/PORT1/$PORT1/g d_cmd.js
-sed -i s/PORT2/$PORT2/g d_cmd.js
-sed -i s/PORT3/$PORT3/g d_cmd.js
-sed -i s/DATABASE_ADMIN_USERNAME/$DATABASE_ADMIN_USERNAME/g d_cmd.js
-sed -i s/DATABASE_ADMIN_PASSWORD/$DATABASE_ADMIN_PASSWORD/g d_cmd.js
 
 ## Create Docker network
 docker network create $DOCKER_NETWORK || true
@@ -157,12 +151,15 @@ sed -i s/"authorization: \"disabled\""/"authorization: \"enabled\""/g ./config/m
 sed -i s/"transitionToAuth: true"/"transitionToAuth: false"/g ./config/mongod.conf
 docker restart mongo1 mongo2 mongo3
 
+ConnectionUrl="mongodb://$DATABASE_ADMIN_USERNAME:$DATABASE_ADMIN_PASSWORD@$SYSTEM_IP:$PORT1,$SYSTEM_IP:$PORT2,$SYSTEM_IP:$PORT3/test?replicaSet=rs0&readPreference=primary&ssl=false&authMechanism=DEFAULT&authSource=admin"
+ProxyConnectionUrl="mongodb://$DATABASE_ADMIN_USERNAME:$DATABASE_ADMIN_PASSWORD@$SYSTEM_IP:$PORT1,$SYSTEM_IP:$PORT2,$SYSTEM_IP:$PORT3/test?replicaSet=rs0&readPreference=primary&ssl=false&authMechanism=DEFAULT&authSource=admin&proxyPort=20170&proxyHost=127.0.0.1"
+
 cat <<EOF > ./config.log
   Connection URL :
     admin url:
-      mongodb://$DATABASE_ADMIN_USERNAME:$DATABASE_ADMIN_PASSWORD@$SYSTEM_IP:$PORT1,$SYSTEM_IP:$PORT2,$SYSTEM_IP:$PORT3/test?replicaSet=rs0&readPreference=primary&ssl=false&authMechanism=DEFAULT&authSource=admin
+      $ConnectionUrl
     proxy:
-      mongodb://$DATABASE_ADMIN_USERNAME:$DATABASE_ADMIN_PASSWORD@$SYSTEM_IP:$PORT1,$SYSTEM_IP:$PORT2,$SYSTEM_IP:$PORT3/test?replicaSet=rs0&readPreference=primary&ssl=false&authMechanism=DEFAULT&authSource=admin&proxyPort=20170&proxyHost=127.0.0.1
+      $ProxyConnectionUrl
   Docker network: $DOCKER_NETWORK
   Username: $DATABASE_ADMIN_USERNAME
   Password: $DATABASE_ADMIN_PASSWORD
